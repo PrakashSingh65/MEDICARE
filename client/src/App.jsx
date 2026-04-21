9f3ddc2ac03b0a6277b17d495370af9b9c95076d
PrakashSingh65 <pratapsing5656@gmail.com>
2026-04-21 23:51:44 +0530

    feat: initialize base application structure with Navbar, App layout, and Home page components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
-     <Outlet />
+      <Navbar />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
