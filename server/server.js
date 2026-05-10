697d9ec7eec7fa4d8607b3dec7f07da03fdbf4dc
PrakashSingh65 <pratapsing5656@gmail.com>
2026-05-10 12:04:21 +0530

    feat: add Dockerfile and docker-compose.yml; update package.json to use module type

--- server/server.js
+++ server/server.js
@@ -5,6 +5,13 @@ import cors from "cors";
 
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
 
 app.use(cors());
 app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on  http://localhost:${PORT}`);
+});
