46dfa72c21e54bfa88e1047798364e9eeb13f501
PrakashSingh65 <pratapsing5656@gmail.com>
2026-05-13 12:42:16 +0530

    feat: integrate Redux and React Query; set up store configuration and update main entry point

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Route/index.jsx";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <>
-    <RouterProvider router={router} />
-  </>,
+
+  <QueryClientProvider client={queryClient}>
+     <RouterProvider router={router} />
+
+  </QueryClientProvider>
+ 
 );
